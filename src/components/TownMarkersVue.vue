f827fbb6fcac4fc39ff97643f799339231c9e1e8
AggelosQubit <aggelosqubit@gmail.com>
2023-02-14 17:26:47 +0100

    first commit

---
+++ src/components/TownMarkersVue.vue
@@ -0,0 +1,34 @@
+<template>
+    <div></div>
+</template>
+  
+<script>
+import mitt from 'mitt'
+import * as THREE from 'three';
+  
+  export default {
+    data() {
+      return {
+        scene: null
+      };
+    },
+    created() {
+      // Listen for the scene-created event from GlobeVue
+      const emitter = mitt()
+        emitter.on('scene-created', (data) => {
+            console.log("scene-created")
+            this.scene = data.scene;
+            this.addMarkers();
+        });
+    },
+    methods: {
+      addMarkers() {
+        // Add markers to the scene
+        const geometry = new THREE.SphereGeometry(0.1, 32, 32);
+        const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+        const marker = new THREE.Mesh(geometry, material);
+        this.scene.add(marker);
+      }
+    }
+  };
+  </script>
